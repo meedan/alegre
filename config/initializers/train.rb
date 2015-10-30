d2ee5979bcbcbc686d92e1a50cd62e13584e7912
Caio SBA <caiosba@gmail.com>
2015-10-30 18:17:31 -0300

    Put the Python libs in a place that RubyPython can find

--- config/initializers/train.rb
+++ config/initializers/train.rb
@@ -4,11 +4,9 @@ MODEL_FILE = "/home/ccx/work/dysl/dysl/corpora/multiLanguage/trainedCorpus2.obj"
 STOPWORDS_PATH = "/home/ccx/work/dysl/dysl/corpora/stopwords"
 
 RubyPython.stop
-#RubyPython.start(:python_exe => "/usr/bin/python")
-RubyPython.start()
+RubyPython.start
 s = RubyPython.import "sys"
 s.path.insert(0, '/home/ccx/work/dysl').rubify
 dysl = RubyPython.import "dysl.langid"
 DYSL = dysl.LangID.new
 DYSL.trainPRELOAD("/home/ccx/work/dysl/dysl/corpora/multiLanguage/trainedCorpus2.obj").rubify
-
