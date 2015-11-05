f18183117ea0bbeff18f850117cdf77ffd629831
Caio SBA <caiosba@gmail.com>
2015-11-04 22:21:49 -0300

    Ticket #4345: Rename Docker image from api_mlg to mlg

--- docker/run.sh
+++ docker/run.sh
@@ -5,14 +5,14 @@ cd $(dirname "${BASH_SOURCE[0]}")
 cd ..
 
 # Build
-docker build -t nlp/api_mlg .
+docker build -t nlp/mlg .
 
 # Run
 secret=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-docker run -d -p 80:80 --name api_mlg nlp/api_mlg
+docker run -d -p 80:80 --name api_mlg nlp/mlg
 
 echo
-docker ps | grep 'api_mlg'
+docker ps | grep mlg
 echo
 
 echo '-----------------------------------------------------------'
