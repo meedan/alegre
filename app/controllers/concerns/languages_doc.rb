5e24000b86cdba8b7febc1bb9c269e6ee25ea5ae
Caio SBA <caiosba@gmail.com>
2015-11-19 18:47:16 -0300

    Using new Lapis version and adding examples to Swagger file

--- app/controllers/concerns/languages_doc.rb
+++ app/controllers/concerns/languages_doc.rb
@@ -10,9 +10,10 @@ module LanguagesDoc
       summary 'Identify the language of a given text'
       notes 'Use this method in order to identify the language of a given text'
       param :query, :text, :string, :required, 'Text to be classified'
-      response :ok, 'Returns text language'
-      response 400, 'Parameters missing (text was not provided)'
-      response 401, 'Access denied'
+      authed = { 'Authorization' => 'Token token="test"' }
+      response :ok, 'Text language', { query: { text: 'The book is on the table' }, headers: authed }
+      response 400, 'Parameter "text" is missing', { query: nil, headers: authed }
+      response 401, 'Access denied', { query: { text: 'Test' } }
     end
 
     swagger_api :sample do
