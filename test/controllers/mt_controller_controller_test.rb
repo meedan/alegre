7137fc200d671b4d666791e7dd17c87f9df2b183
Caio SBA <caiosba@gmail.com>
2016-02-19 04:07:10 -0300

    Ticket #4274: Machine Translations API, part 2: Automated tests

--- test/controllers/mt_controller_controller_test.rb
+++
@@ -1,9 +0,0 @@
-#encoding: utf-8
-require File.join(File.expand_path(File.dirname(__FILE__)), '..', 'test_helper')
-
-class MtControllerControllerTest < ActionController::TestCase
-  def setup
-    super
-    @controller = Api::V1::MtController.new
-  end
-end
