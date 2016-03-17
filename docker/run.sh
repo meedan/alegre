c1916f20d716486dbf70b738a47a8b4636fda80e
infojunkie <karim.ratib@gmail.com>
2016-03-17 11:48:20 -0700

    Update docker

--- docker/run.sh
+++ docker/run.sh
@@ -14,10 +14,10 @@ cd ..
 docker build -t dreg.meedan.net/nlp/alegre .
 
 # Run
-docker run -d -p 80:80 --name $NAME dreg.meedan.net/nlp/alegre
+docker run -d -p 8000:80 --name $NAME dreg.meedan.net/nlp/alegre
 
 echo '-----------------------------------------------------------'
-echo 'Now go to your browser and access http://<hostname>/api'
+echo 'Now go to your browser and access http://localhost:8000/api'
 echo '-----------------------------------------------------------'
 
 cd $dir
