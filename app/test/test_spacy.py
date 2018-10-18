a08d04d36ff1ef5bc443cba5bab6cad39661072f
infojunkie <karim.ratib@gmail.com>
2018-10-18 13:57:09 -0700

    spaCy test

--- app/test/test_spacy.py
+++ app/test/test_spacy.py
@@ -4,21 +4,23 @@ from app.main import db
 import json
 from app.test.base import BaseTestCase
 
-class TestLangidBlueprint(BaseTestCase):
-    def test_langid(self):
+class TestSpacyBlueprint(BaseTestCase):
+    maxDiff = None
+
+    def test_spacy(self):
         with self.client:
             response = self.client.post(
-                '/langid/',
+                '/spacy/',
                 data=json.dumps(dict(
-                    text='Hello this is a test'
+                    text='Pastafarians are smarter than people with Coca Cola bottles.',
+                    model='en'
                 )),
                 content_type='application/json'
             )
-            data = json.loads(response.data.decode())
-            self.assertTrue(data['language'] == 'en')
-            self.assertTrue(data['confidence'] == -75.6393435001)
             self.assertTrue(response.content_type == 'application/json')
             self.assertEqual(response.status_code, 200)
+            data = json.loads(response.data.decode())
+            self.assertDictEqual(data, json.load(open('./app/test/spacy.json')))
 
 
 if __name__ == '__main__':
