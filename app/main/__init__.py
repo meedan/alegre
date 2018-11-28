4f1cb5662d81173f8e4a0bdf5c6873cbc9881b78
Caio Almeida <caiosba@gmail.com>
2018-11-28 05:40:25 -0300

    Ticket #7191: Make it work with HTTPS

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from elasticsearch import Elasticsearch, TransportError
+from werkzeug.contrib.fixers import ProxyFix
 import json
 
 from .config import config_by_name
@@ -12,6 +13,7 @@ flask_bcrypt = Bcrypt()
 def create_app(config_name):
   app = Flask(__name__)
   app.config.from_object(config_by_name[config_name])
+  app.wsgi_app = ProxyFix(app.wsgi_app)
   db.init_app(app)
   flask_bcrypt.init_app(app)
 
