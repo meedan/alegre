9bfbdad81d66ed903c8370e52e526add34746e8a
infojunkie <karim.ratib@gmail.com>
2020-02-17 09:53:43 -0300

    Test bit_count

--- app/test/test_image_similarity.py
+++ app/test/test_image_similarity.py
@@ -14,7 +14,15 @@ class TestImageSimilaryBlueprint(BaseTestCase):
     p1 = compute_phash_int(im1)
     im2 = Image.open('./app/test/data/lenna-512.png').convert('RGB')
     p2 = compute_phash_int(im2)
+    im3 = Image.open('./app/test/data/lenna-256.png').convert('RGB')
+    p3 = compute_phash_int(im3)
     self.assertEqual(p1, p2)
+    self.assertEqual(p1, p3)
+
+  def test_bit_count(self):
+    p = 45655524591978137 # the hash of the image above
+    result = db.session.execute("SELECT BIT_COUNT(:p) AS test_count", { 'p': p }).first()
+    self.assertEqual(result['test_count'], 28)
 
 
 if __name__ == '__main__':
