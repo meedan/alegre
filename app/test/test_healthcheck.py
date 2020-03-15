ea81b7a9daac07a1cac02274eaca00190d668731
infojunkie <karim.ratib@gmail.com>
2020-03-15 15:33:58 -0700

    Healthcheck unit tests, part deux

--- app/test/test_healthcheck.py
+++ app/test/test_healthcheck.py
@@ -13,5 +13,26 @@ class TestHealthcheckBlueprint(BaseTestCase):
     self.assertEqual(200, response.status_code)
     self.assertEqual(True, all(result['result']))
 
+
+class TestHealthcheckBlueprintWithBadConfig(BaseTestCase):
+  def setUp(self):
+    # Inject some bad config here
+    # but don't call parent
+    app.config.update({
+      'ELASTICSEARCH_URL': 'bad',
+      'REDIS_HOST': 'bad',
+      'SQLALCHEMY_DATABASE_URI': 'bad'
+    })
+
+  def tearDown(self):
+    # Don't call parent
+    pass
+
+  def test_healthcheck_api_with_bad_config(self):
+    response = self.client.get('/healthcheck/')
+    result = json.loads(response.data.decode())
+    self.assertEqual('application/json', response.content_type)
+    self.assertEqual(500, response.status_code)
+
 if __name__ == '__main__':
     unittest.main()
