da471849c47733bf6ca2d1ec7ee7487285733241
Caio Almeida <caiosba@gmail.com>
2020-03-16 17:45:47 -0300

    Copy configuration files from configurator

--- docker/bin/start.sh
+++ docker/bin/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /app
+if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
+d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
 make run &
 nginx -g 'daemon off;'
