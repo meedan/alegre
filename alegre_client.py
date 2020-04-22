5a836bb7404953c72d6468d5d75ab3bfb0946ff3
Devin Gaffney <itsme@devingaffney.com>
2020-04-22 11:27:46 -0400

    refactor client to allow for the various tests we're now running

--- alegre_client.py
+++ alegre_client.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import csv
+import random
 from collections import Counter
 class AlegreClient:
 
@@ -12,7 +13,7 @@ class AlegreClient:
   def text_similarity_path():
     return "/text/similarity/"
 
-  def fact_pairs_from_csv(self, filename, threshold):
+  def fact_pairs_from_csv(self, filename="texts.csv", threshold=4):
     # Expects filename to refer to local CSV path where columns are [similarity_score],[text_1],[text_2]
     with open(filename) as f:
       reader = csv.reader(f)
@@ -20,7 +21,7 @@ class AlegreClient:
       for row in reader:
         score = float(row[0])
         if score >= threshold:
-          dataset.append({"fact_pair": [row[1], row[2]], "score": score})
+          dataset.append({"database_text": row[1], "lookup_text": row[2], "score": score})
     return dataset
 
   def __init__(self, hostname=None):
@@ -35,14 +36,28 @@ class AlegreClient:
         request_params["context"] = context
       self.store_text_similarity(request_params)
 
-  def input_confounders(self, confounder_filename, model_name, context):
+  def load_confounder_paragraphs(self, confounder_filename="train-v2.0.json"):
+    paragraphs = []
+    for row in json.loads(open(confounder_filename).read())["data"]:
+      for para in row["paragraphs"]:
+        if random.random() < 0.5:
+          paragraphs.append(para["context"])
+    return paragraphs
+
+  def load_confounder_headlines(self, confounder_filename="confounder_headlines.csv"):
+    confounders = []
     with open(confounder_filename) as f:
       reader = csv.reader(f)
       for row in reader:
-        request_params = {"model": model_name, "text": row[0].lower()}
-        if context:
-          request_params["context"] = context
-        self.store_text_similarity(request_params)
+        confounders.append(row[0].lower())
+    return confounders
+
+  def input_confounders(self, confounders, model_name, context):
+    for row in confounders:
+      request_params = {"model": model_name, "text": row}
+      if context:
+        request_params["context"] = context
+      self.store_text_similarity(request_params)
 
   def get_for_text(text, model_name, context={}):
     if not context:
@@ -54,44 +69,36 @@ class AlegreClient:
       "threshold": 0.0,
     }).text)
 
-  def evaluate_model(self, filename, model_name, confounder_filename=None, store_dataset=True, omit_half=False, threshold=4):
+  def evaluate_model(self, dataset, model_name, confounders, store_dataset, omit_half):
     # Similarity score should be from 0 to 5 similar to data found at https://ixa2.si.ehu.es/stswiki/index.php/STSbenchmark,
     # text_1 will be uploaded to service, text_2 will be used to attempt to retrieve text_1 from the service in a GET request.
-    dataset = self.fact_pairs_from_csv(filename, threshold)
     split_point = int(len(dataset)/2)
     context = {"task": "model_evaluation", "model_name": model_name}
     if store_dataset:
-      if confounder_filename:
-        self.input_confounders(confounder_filename, model_name, context)
+      if confounders:
+        self.input_confounders(confounders, model_name, context)
       if omit_half:
         sent_group = dataset[:split_point]
       else:
         sent_group = dataset
       self.input_cases(
-        [f["fact_pair"][0].lower() for f in sent_group],
+        [f["database_text"].lower() for f in sent_group],
         model_name,
         context
       )
-    report = {"count": 0, "success": 0, "successes": [], "fails": [], "server_errors": 0, "resultset": []}
+    results = {"count": 0, "success": 0, "server_errors": 0, "resultset": []}
     for ii, fact_pair in enumerate(dataset):
-      report["count"] += 1
+      results["count"] += 1
       result = json.loads(self.get_similar_texts({
         "model": model_name,
-        "text": fact_pair["fact_pair"][1].lower(),
+        "text": fact_pair["lookup_text"].lower(),
         "context": context,
         "threshold": 0.0,
       }).text)
       #due to indexing math this may be an off-by-one but it's close enough for our test today, and it's too late at night to make sure, but not too late so as not to remind myself about this later....
       is_omitted = ii > split_point
-      report["resultset"].append({"fact_pair": fact_pair, "response": result, "is_omitted": is_omitted})
-      if result and result.get("result") and result["result"][0]["_source"]["content"].lower() == fact_pair["fact_pair"][0].lower():
-        report["success"] += 1
-        report["successes"].append(fact_pair)
-      elif result.get("message") == "Internal Server Error":
-        report["server_errors"] += 1
-      else:
-        report["fails"].append(fact_pair)
-    return report
+      results["resultset"].append({"fact_pair": fact_pair, "response": result, "is_omitted": is_omitted})
+    return results
 
   def store_text_similarity(self, request_params):
     return requests.post(self.hostname+self.text_similarity_path(), json=request_params)
@@ -99,51 +106,78 @@ class AlegreClient:
   def get_similar_texts(self, request_params):
     return requests.get(self.hostname+self.text_similarity_path(), json=request_params)
 
-def interpret_report(report):
-  positions = Counter()
-  results_dataset = []
-  results_dataset.append(["Database-Stored Sentence", "Lookup Sentence", "Top Yielded Sentence", "ES Similarity Score", "Result Status"])
-  for res in report["resultset"]:
-    if not res.get('response', {}).get("message"):
-      competing_sentences = [ee.get("_source", {}).get("content").lower() for ee in res.get("response", {}).get("result")]
-      db_sentence = res.get("fact_pair").get("fact_pair")[0].lower()
-      lookup_sentence = res.get("fact_pair").get("fact_pair")[1].lower()
-      row = [db_sentence, lookup_sentence]
-      if competing_sentences and db_sentence == competing_sentences[0]:
-        positions.update([1])
-        row.append(competing_sentences[0])
-        row.append(res.get("response", {}).get("result")[0].get("_score"))
-        row.append("Success")
-      elif competing_sentences and db_sentence != competing_sentences[0] and db_sentence in competing_sentences:
-        positions.update([competing_sentences.index(db_sentence)+1])
-        row.append(competing_sentences[0])
-        row.append(res.get("response", {}).get("result")[0].get("_score"))
-        row.append("Partial Success")
-      elif not competing_sentences:
-        if res.get("is_omitted"):
-          positions.update(["true negative"])
-          row.append("Nothing found!")
-          row.append("Nothing found!")
-          row.append("True Negative")
+  def transform_stanford_qa_data(self, qa_filename="dev-v2.0.json"):
+    #we may want to limit the number of cases per paragraph we look at here - 
+    #the groups of paragraphs are all thematically grouped, so results often 
+    #yield "good" answers even though they refer to some other similar paragraph in the set.
+    dataset = json.loads(open(qa_filename).read())
+    paired_qas = []
+    for row in dataset["data"]:
+      for paragraph in row["paragraphs"]:
+        for question in paragraph["qas"]:
+          paired_qas.append({"lookup_text": question.get("question"), "database_text": paragraph.get("context")})
+    return paired_qas
+
+  def run_b_test(self, model_name):
+    return self.evaluate_model(self.fact_pairs_from_csv(), model_name, [], True, False)
+
+  def run_c_test(self, model_name):
+    return self.evaluate_model(self.fact_pairs_from_csv(), model_name, self.load_confounder_headlines(), True, False)
+
+  def run_cwm_test(self, model_name):
+    return self.evaluate_model(self.fact_pairs_from_csv(), model_name, self.load_confounder_headlines(), True, True)
+
+  def run_qa_test(self, model_name):
+    return self.evaluate_model(self.transform_stanford_qa_data(), model_name, [], True, False)
+
+  def run_qawm_test(self, model_name):
+    return self.evaluate_model(self.transform_stanford_qa_data(), model_name, self.load_confounder_paragraphs(), True, False)
+
+  def interpret_report(self, report):
+    positions = Counter()
+    results_dataset = []
+    results_dataset.append(["Database-Stored Sentence", "Lookup Sentence", "Top Yielded Sentence", "ES Similarity Score", "Result Status"])
+    for res in report["resultset"]:
+      if not res.get('response', {}).get("message"):
+        competing_sentences = [ee.get("_source", {}).get("content").lower() for ee in res.get("response", {}).get("result")]
+        db_sentence = res.get("fact_pair", {}).get("database_text", "").lower()
+        lookup_sentence = res.get("fact_pair", {}).get("lookup_text", "").lower()
+        row = [db_sentence, lookup_sentence]
+        if competing_sentences and db_sentence == competing_sentences[0]:
+          positions.update([1])
+          row.append(competing_sentences[0])
+          row.append(res.get("response", {}).get("result")[0].get("_score"))
+          row.append("Success")
+        elif competing_sentences and db_sentence != competing_sentences[0] and db_sentence in competing_sentences:
+          positions.update([competing_sentences.index(db_sentence)+1])
+          row.append(competing_sentences[0])
+          row.append(res.get("response", {}).get("result")[0].get("_score"))
+          row.append("Partial Success")
+        elif not competing_sentences:
+          if res.get("is_omitted"):
+            positions.update(["true negative"])
+            row.append("Nothing found!")
+            row.append("Nothing found!")
+            row.append("True Negative")
+          else:
+            positions.update(["false negative"])
+            row.append("Nothing found!")
+            row.append("Nothing found!")
+            row.append("False Negative")
         else:
-          positions.update(["false negative"])
-          row.append("Nothing found!")
-          row.append("Nothing found!")
-          row.append("False Negative")
-      else:
-        positions.update(["false positive"])
-        row.append(competing_sentences[0])
-        row.append(res.get("response", {}).get("result")[0].get("_score"))
-        row.append("False Positive")
-      results_dataset.append(row)
-    else: 
-      positions.update(["server error"])
-  return results_dataset, positions
+          positions.update(["false positive"])
+          row.append(competing_sentences[0])
+          row.append(res.get("response", {}).get("result")[0].get("_score"))
+          row.append("False Positive")
+        results_dataset.append(row)
+      else: 
+        positions.update(["server error"])
+    return results_dataset, positions
 
 if __name__ == '__main__':
-  # from alegre_client import AlegreClient
+  #from alegre_client import AlegreClient
   ac = AlegreClient()
-  report = ac.evaluate_model("texts.csv", "universal-sentence-encoder-large", "confounder_headlines.csv", True, True)
+  report = ac.run_qa_test("elasticsearch")
   results_dataset, positions = interpret_report(report)
   print(positions)
   with open('alegre_fact_recall_report.csv', 'w', newline='') as f:
