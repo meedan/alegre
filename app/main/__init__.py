b857f301a98eb0314ba565d4eb1b1d5d4b3cc8cc
infojunkie <karim.ratib@gmail.com>
2020-05-07 12:57:41 -0700

    Better retry, JSON logging #8282

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 from werkzeug.contrib.fixers import ProxyFix
 import pybrake.flask
 import logging
+import json_logging
 from .config import config_by_name
 
 db = SQLAlchemy()
@@ -26,6 +27,11 @@ def create_app(config_name):
   db.init_app(app)
   flask_bcrypt.init_app(app)
   migrate.init_app(app, db)
+
+  # Init JSON logging, only once to avoid exceptions during tests
+  if json_logging._current_framework is None:
+    json_logging.init_flask(enable_json=True)
+    json_logging.init_request_instrument(app)
 
   with app.app_context():
     if app.config['PYBRAKE']['project_key']:
