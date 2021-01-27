eb24e622476e55ee481bc8feb92807e8da91ad77
Martin Peck <martin@meedan.com>
2021-01-27 16:10:53 -0700

    Add a second worker to gunicorn configuration for redundant API capacity. See if ELB 500 error rate affected.

--- production/bin/start.sh
+++ production/bin/start.sh
@@ -13,4 +13,4 @@ set +o allexport
 
 python manage.py init
 python manage.py db upgrade
-gunicorn -w 1 -b 0.0.0.0:${ALEGRE_PORT} --access-logfile - --error-logfile - wsgi:app
+gunicorn -w 2 --threads 2 -b 0.0.0.0:${ALEGRE_PORT} --access-logfile - --error-logfile - wsgi:app
