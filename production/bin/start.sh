c39df9cc4a35b956cd67f4b2ad07801d9d05bd21
Martin Peck <martin@meedan.com>
2021-01-27 19:27:15 -0700

    Restore configurator copy.

--- production/bin/start.sh
+++ production/bin/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /app
+if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
+d=configurator/check/${DEPLOY_ENV}/alegre/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
 
 set -o allexport
 if [ -z "$SSM" ]
