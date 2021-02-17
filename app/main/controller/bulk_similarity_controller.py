7f46971033b1e8a7b34be1882c4d772edf738a93
Devin Gaffney <itsme@devingaffney.com>
2021-02-17 16:26:22 -0500

    CHECK-176 refactor for actual bulk ops

--- app/main/controller/bulk_similarity_controller.py
+++ app/main/controller/bulk_similarity_controller.py
@@ -1,6 +1,7 @@
 from flask import request, current_app as app
 from flask_restplus import Resource, Namespace, fields
 from elasticsearch import Elasticsearch
+from elasticsearch import helpers
 import elasticsearch
 from app.main.lib.fields import JsonObject
 from app.main.lib.elasticsearch import language_to_analyzer
@@ -11,8 +12,20 @@ api = Namespace('bulk_similarity', description='bulk text similarity operations'
 similarity_request = api.model('bulk_similarity_request', {
     'documents': fields.List(required=True, description='List of individual parameters typically sent to the similarity controller', cls_or_instance=JsonObject)
 })
+def each_slice(list, size):
+    batch = 0
+    while batch * size < len(list):
+        yield list[batch * size:(batch + 1) * size]
+        batch += 1
+
 @api.route('/')
 class BulkSimilarityResource(Resource):
+    def get_bulk_write_object(self, doc_id, body):
+        return dict(
+            **{'_index': app.config['ELASTICSEARCH_SIMILARITY'], '_type': '_doc', '_id': doc_id},
+            **body
+        )
+
     def get_bodies_for_request(self):
         bodies = []
         doc_ids = []
@@ -49,8 +62,11 @@ class BulkSimilarityResource(Resource):
     def post(self):
         doc_ids, bodies = self.get_bodies_for_request()
         es = Elasticsearch(app.config['ELASTICSEARCH_URL'])
-        results = []
         sim_controller = SimilarityResource()
-        for doc_id, body in zip(doc_ids, bodies):
-            results.append(sim_controller.store_document(body, doc_id))
+        results = []
+        for doc_id_set, body_set in each_slice(zip(doc_ids, bodies), 8000):
+            to_write = []
+            for doc_id, body in zip(doc_id_set, body_set):
+                to_write.append(self.get_bulk_write_object(doc_id, body))
+            results.append(helpers.bulk(es, to_write))
         return results
