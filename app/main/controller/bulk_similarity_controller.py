002dd8512e7427e5aa05a16ab2f5d609fff42d56
Devin Gaffney <itsme@devingaffney.com>
2021-02-17 19:44:07 -0500

    CHECK-176 refactor for actual bulk ops (#54)
    * CHECK-176 refactor for actual bulk ops
    
    * yield full zip
    
    * yield full zip
    
    * updates
    
    * updates
    
    * remove unnecessary endpoints

--- app/main/controller/bulk_similarity_controller.py
+++ app/main/controller/bulk_similarity_controller.py
@@ -1,18 +1,30 @@
 from flask import request, current_app as app
 from flask_restplus import Resource, Namespace, fields
 from elasticsearch import Elasticsearch
+from elasticsearch import helpers
 import elasticsearch
 from app.main.lib.fields import JsonObject
 from app.main.lib.elasticsearch import language_to_analyzer
 from app.main.lib.shared_models.shared_model import SharedModel
-from app.main.controller.similarity_controller import SimilarityResource
 
 api = Namespace('bulk_similarity', description='bulk text similarity operations')
 similarity_request = api.model('bulk_similarity_request', {
     'documents': fields.List(required=True, description='List of individual parameters typically sent to the similarity controller', cls_or_instance=JsonObject)
 })
+def each_slice(list, size):
+    batch = 0
+    while batch * size < len(list):
+        yield list[batch * size:(batch + 1) * size]
+        batch += 1
+
 @api.route('/')
 class BulkSimilarityResource(Resource):
+    def get_bulk_write_object(self, doc_id, body):
+        return dict(
+            **{'_index': app.config['ELASTICSEARCH_SIMILARITY'], '_type': '_doc', '_id': doc_id},
+            **body
+        )
+
     def get_bodies_for_request(self):
         bodies = []
         doc_ids = []
@@ -32,16 +44,6 @@ class BulkSimilarityResource(Resource):
             doc_ids.append(document.get("doc_id"))
             bodies.append(body)
         return doc_ids, bodies
-
-    @api.response(200, 'text successfully deleted in the similarity database.')
-    @api.doc('Delete a text in the similarity database')
-    @api.expect(similarity_request, validate=True)
-    def delete(self):
-        results = []
-        sim_controller = SimilarityResource()
-        for doc in request.json["documents"]:
-            results.append(sim_controller.delete_document(doc["doc_id"]))
-        return results
         
     @api.response(200, 'text successfully stored in the similarity database.')
     @api.doc('Store a text in the similarity database')
@@ -50,7 +52,9 @@ class BulkSimilarityResource(Resource):
         doc_ids, bodies = self.get_bodies_for_request()
         es = Elasticsearch(app.config['ELASTICSEARCH_URL'])
         results = []
-        sim_controller = SimilarityResource()
-        for doc_id, body in zip(doc_ids, bodies):
-            results.append(sim_controller.store_document(body, doc_id))
+        for doc_body_set in each_slice(list(zip(doc_ids, bodies)), 8000):
+            to_write = []
+            for doc_id, body in doc_body_set:
+                to_write.append(self.get_bulk_write_object(doc_id, body))
+            results.append(helpers.bulk(es, to_write))
         return results
--- app/test/test_bulk_similarity.py
+++ app/test/test_bulk_similarity.py
@@ -39,48 +39,11 @@ class TestBulkSimilarityBlueprint(BaseTestCase):
         mapping[app.config['ELASTICSEARCH_SIMILARITY']]['mappings']
       )
 
-    def test_elasticsearch_update_text(self):
-        with self.client:
-            term = { 'text': 'how to slice a banana', 'model': 'elasticsearch', 'context': { 'dbid': 54 } }
-            post_response = self.client.post('/text/bulk_similarity/', data=json.dumps({"documents": [term]}), content_type='application/json')
-            es = Elasticsearch(app.config['ELASTICSEARCH_URL'])
-            results = es.search(body={"query": {"match_all": {}}},index=app.config['ELASTICSEARCH_SIMILARITY'])
-            doc = [e for e in results["hits"]["hits"] if e["_source"]['content'] == term['text']][0]
-            term2 = { 'text': 'how to slice a pizza', 'model': 'elasticsearch', 'context': { 'dbid': 54 }, 'doc_id': doc["_id"]}
-            post_response2 = self.client.post('/text/bulk_similarity/', data=json.dumps({"documents": [term2]}), content_type='application/json')
-            results = es.search(body={"query": {"match_all": {}}},index=app.config['ELASTICSEARCH_SIMILARITY'])
-            doc = [e for e in results["hits"]["hits"] if doc["_id"] == e["_id"]][0]
-            self.assertEqual(term2['text'], doc['_source']['content'])
-
-    def test_elasticsearch_update_text_with_doc_id(self):
+    def test_elasticsearch_insert_text_with_doc_id(self):
         with self.client:
             term = { 'text': 'how to slice a banana', 'model': 'elasticsearch', 'context': { 'dbid': 54 }, 'doc_id': "123456" }
             post_response = self.client.post('/text/bulk_similarity/', data=json.dumps({"documents": [term]}), content_type='application/json')
-            es = Elasticsearch(app.config['ELASTICSEARCH_URL'])
-            results = es.search(body={"query": {"match_all": {}}},index=app.config['ELASTICSEARCH_SIMILARITY'])
-            doc = [e for e in results["hits"]["hits"] if e["_source"]['content'] == term['text']][0]
-            term2 = { 'text': 'how to slice a pizza', 'model': 'elasticsearch', 'context': { 'dbid': 54 }, 'doc_id': "123456"}
-            post_response2 = self.client.post('/text/bulk_similarity/', data=json.dumps({"documents": [term2]}), content_type='application/json')
-            results = es.search(body={"query": {"match_all": {}}},index=app.config['ELASTICSEARCH_SIMILARITY'])
-            doc = [e for e in results["hits"]["hits"] if doc["_id"] == e["_id"]][0]
-            self.assertEqual(term2['text'], doc['_source']['content'])
-
-    def test_elasticsearch_delete_text(self):
-        with self.client:
-            term = { 'text': 'how to slice a banana', 'model': 'elasticsearch', 'context': { 'dbid': 54 } }
-            post_response = self.client.post('/text/bulk_similarity/', data=json.dumps({"documents": [term]}), content_type='application/json')
-            result = json.loads(post_response.data.decode())
-            self.assertEqual(True, result[0]['success'])
-            es = Elasticsearch(app.config['ELASTICSEARCH_URL'])
-            results = es.search(body={"query": {"match_all": {}}},index=app.config['ELASTICSEARCH_SIMILARITY'])
-            doc = [e for e in results["hits"]["hits"] if e["_source"]['content'] == term['text']][0]
-            delete_response = self.client.delete(
-                '/text/bulk_similarity/',
-                data=json.dumps({"documents": [{"doc_id": doc["_id"]}]}),
-                content_type='application/json'
-            )
-            result = json.loads(delete_response.data.decode())
-            self.assertEqual('deleted', result[0]['result'])
+            self.assertTrue(post_response)
 
 if __name__ == '__main__':
     unittest.main()
