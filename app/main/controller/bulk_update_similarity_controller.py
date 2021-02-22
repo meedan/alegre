3aa3d83aa1ce6edce11c8beaa55ab552ee90a302
Devin Gaffney <itsme@devingaffney.com>
2021-02-22 10:35:55 -0500

    CHECK-277 update alegre to allow for bulk updating to generate vectors

--- app/main/controller/bulk_update_similarity_controller.py
+++ app/main/controller/bulk_update_similarity_controller.py
@@ -6,9 +6,10 @@ import elasticsearch
 from app.main.lib.fields import JsonObject
 from app.main.lib.elasticsearch import language_to_analyzer
 from app.main.lib.shared_models.shared_model import SharedModel
+from app.main.controller.bulk_similarity_controller import BulkSimilarityController
 
-api = Namespace('bulk_similarity', description='bulk text similarity operations')
-similarity_request = api.model('bulk_similarity_request', {
+api = Namespace('bulk_update_similarity', description='bulk text similarity operations')
+similarity_request = api.model('bulk_update_similarity_request', {
     'documents': fields.List(required=True, description='List of individual parameters typically sent to the similarity controller', cls_or_instance=JsonObject)
 })
 def each_slice(list, size):
@@ -18,10 +19,10 @@ def each_slice(list, size):
         batch += 1
 
 @api.route('/')
-class BulkSimilarityResource(Resource):
+class BulkUpdateSimilarityResource(Resource):
     def get_bulk_write_object(self, doc_id, body):
         return dict(
-            **{'_index': app.config['ELASTICSEARCH_SIMILARITY'], '_type': '_doc', '_id': doc_id},
+            **{'_op_type': 'update', '_index': app.config['ELASTICSEARCH_SIMILARITY'], '_type': '_doc', '_id': doc_id},
             **body
         )
 
@@ -29,36 +30,20 @@ class BulkSimilarityResource(Resource):
         bodies = []
         doc_ids = []
         for document in request.json.get("documents", []):
-            model_key = 'elasticsearch'
-            if 'model' in document:
-                model_key = document['model']
             es = Elasticsearch(app.config['ELASTICSEARCH_URL'])
-            body = { 'content': document['text'] }
-            if model_key.lower() != 'elasticsearch':
-                model = SharedModel.get_client(model_key)
-                vector = model.get_shared_model_response(document['text'])
-                body['vector_'+str(len(vector))] = vector
-                body['model'] = model_key
+            body = {'model': model_key}
+            model = SharedModel.get_client(model_key)
+            vector = model.get_shared_model_response(document['text'])
+            body['vector_'+str(len(vector))] = vector
             if 'context' in document:
                 body['context'] = document['context']
             doc_ids.append(document.get("doc_id"))
             bodies.append(body)
         return doc_ids, bodies
         
-    def submit_bulk_request(self, doc_ids, bodies):
-        es = Elasticsearch(app.config['ELASTICSEARCH_URL'])
-        writables = []
-        for doc_body_set in each_slice(list(zip(doc_ids, bodies)), 8000):
-            to_write = []
-            for doc_id, body in doc_body_set:
-                to_write.append(self.get_bulk_write_object(doc_id, body))
-                writables.append(to_write[-1])
-            helpers.bulk(es, to_write)
-        return writables
-
     @api.response(200, 'text successfully stored in the similarity database.')
     @api.doc('Store a text in the similarity database')
     @api.expect(similarity_request, validate=True)
     def post(self):
         doc_ids, bodies = self.get_bodies_for_request()
-        return self.submit_bulk_request(doc_ids, bodies)
+        return BulkSimilarityController().submit_bulk_request(doc_ids, bodies)
