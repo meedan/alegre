d33ab4a8df3b233aecc9353883dd78e5ddab173e
Devin Gaffney <itsme@devingaffney.com>
2021-07-22 06:59:36 -0700

    Tweak for bug

--- app/main/lib/shared_models/video_model.py
+++ app/main/lib/shared_models/video_model.py
@@ -144,7 +144,7 @@ class VideoModel(SharedModel):
             temp_comparison_file = self.get_tempfile()
             with open(temp_search_file.name, 'w') as out_file:
                 out_file.write(str.join("\n", self.get_fullpath_files(matches, False)))
-            temp_filtered_search_file = narrow_search_to_existing_files(temp_search_file)
+            temp_filtered_search_file = self.narrow_search_to_existing_files(temp_search_file)
             with open(temp_comparison_file.name, 'w') as out_file:
                 out_file.write(self.tmk_file_path(video.folder, video.filepath))
             tmk_query_command = self.tmk_query_command()
