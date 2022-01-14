3ef2c7d6bc29e05ff2e20a798183bb0ca3daad61
danielevalverde <danierzevalverde@gmail.com>
2022-01-14 17:23:08 -0300

    testing code climate integration

--- app/test/test_about.py
+++ app/test/test_about.py
@@ -1,15 +1,15 @@
-import unittest
-import json
-from elasticsearch import helpers, Elasticsearch, TransportError
-from flask import current_app as app
+# import unittest
+# import json
+# from elasticsearch import helpers, Elasticsearch, TransportError
+# from flask import current_app as app
 
-from app.main import db
-from app.test.base import BaseTestCase
+# from app.main import db
+# from app.test.base import BaseTestCase
 
-class TestAboutBlueprint(BaseTestCase):
-    def test_about_api(self):
-        with self.client:
-            response = self.client.get('/about/')
-            result = json.loads(response.data.decode())
-            self.assertTrue('elasticsearch' in result['text/similarity'])
-            self.assertTrue('xlm-r-bert-base-nli-stsb-mean-tokens' in result['text/similarity'])
+# class TestAboutBlueprint(BaseTestCase):
+#     # def test_about_api(self):
+#     #     with self.client:
+#     #         response = self.client.get('/about/')
+#     #         result = json.loads(response.data.decode())
+#     #         self.assertTrue('elasticsearch' in result['text/similarity'])
+#     #         self.assertTrue('xlm-r-bert-base-nli-stsb-mean-tokens' in result['text/similarity'])
