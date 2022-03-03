dc9037860b89934a6008ca850a7bfabc61d8657a
danielevalverde <danierzevalverde@gmail.com>
2022-03-03 19:58:17 -0300

    add unit test and fix retry

--- app/test/test_ocr.py
+++ app/test/test_ocr.py
@@ -1,10 +1,11 @@
 import unittest
 import json
 from google.cloud import vision
-
+from unittest.mock import patch
 from app.main import db
 from app.main.lib.google_client import get_credentialed_google_client
 from app.test.base import BaseTestCase
+from google.cloud import vision
 
 class TestOcrBlueprint(BaseTestCase):
     def test_ocr_api_successful(self):
@@ -42,6 +43,19 @@ class TestOcrBlueprint(BaseTestCase):
             )
             result = json.loads(response.data.decode())
             self.assertIsNone(result)
+
+    def test_ocr_google_api_error(self):
+        with patch('app.main.controller.image_ocr_controller.CLIENT') as client:
+            client.return_value= Exception('We can not access the URL currently. Please download the content and pass it in')
+            response = self.client.get(
+                '/image/ocr/',
+                data=json.dumps({
+                'url': 'https://i.imgur.com/ewGClFQ.png',
+                }),
+                content_type='application/json'
+            )
+            result = json.loads(response.data.decode())
+            self.assertIn('Error', result['message'])
 
 if __name__ == '__main__':
     unittest.main()
