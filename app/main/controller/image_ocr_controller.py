754bae9f7aafef3eaa8677550ee11dbc0fdf6ec1
danielevalverde <danierzevalverde@gmail.com>
2022-03-03 12:18:06 -0300

    retry when get error 'We can not access the URL currently. Please download the content and pass it in'

--- app/main/controller/image_ocr_controller.py
+++ app/main/controller/image_ocr_controller.py
@@ -1,6 +1,7 @@
 from flask import request, current_app as app
 from flask_restplus import Resource, Namespace, fields
 from google.cloud import vision
+import tenacity
 
 from app.main.lib.google_client import get_credentialed_google_client
 
@@ -9,11 +10,15 @@ ocr_request = api.model('ocr_request', {
     'url': fields.String(required=True, description='url of image to extract text from'),
 })
 
+def _after_log(retry_state):
+    app.logger.debug("Retrying text extraction...")
 @api.route('/')
 class ImageOcrResource(Resource):
     @api.response(200, 'text successfully extracted.')
     @api.doc('Perform text extraction from an image')
     @api.doc(params={'url': 'url of image to extract text from'})
+
+    @tenacity.retry(wait=tenacity.wait_exponential(multiplier=1, min=0, max=5), stop=tenacity.stop_after_attempt(20), after=_after_log, reraise=True)
     def get(self):
         client = get_credentialed_google_client(vision.ImageAnnotatorClient)
 
@@ -24,6 +29,10 @@ class ImageOcrResource(Resource):
             image.source.image_uri = request.json['url']
 
         response = client.document_text_detection(image=image)
+
+        if response.error:
+            raise Exception("we can not access the URL currently. Please download the content and pass it in")
+
         texts = response.text_annotations
 
         if not texts:
