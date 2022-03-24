c5e472464cda2f97c81bfab4cc914090a0f17809
Devin Gaffney <itsme@devingaffney.com>
2022-03-24 04:50:42 -0700

    CHECK-1618 add tests and fix logic

--- app/main/lib/helpers.py
+++ app/main/lib/helpers.py
@@ -1,7 +1,7 @@
 def context_matches(query_context, item_context):
   for k,v in query_context.items():
-    if type(v) is list:
-      if item_context.get(k) != v or v not in item_context.get(k):
+    if isinstance(v, list):
+      if item_context.get(k) != v and item_context.get(k) not in v:
         return False
     else:
       if item_context.get(k) != v:
