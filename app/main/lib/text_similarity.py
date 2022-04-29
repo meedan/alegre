73da91595920f00451fa33d11bc57b2c4909f245
Devin Gaffney <itsme@devingaffney.com>
2022-04-29 13:57:36 -0700

    CHECK-1795 maybe this does it

--- app/main/lib/text_similarity.py
+++ app/main/lib/text_similarity.py
@@ -20,12 +20,13 @@ def add_text(body, doc_id):
     return documents
 
 def search_text(search_params):
-  results = []
+  results = {"result": []}
   for model_key in search_params.pop("models", []):
     result = search_text_by_model(dict(**search_params, **{'model': model_key}))
     if 'error' in result:
       return result, 500
-    results.append(result)
+    for search_result in result["result"]:
+      results["result"].append(search_result)
   return results
 
 def search_text_by_model(search_params):
