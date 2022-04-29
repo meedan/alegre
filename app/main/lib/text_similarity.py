f96dca3963d5733b767d4576e91ba3f451bcaa25
Devin Gaffney <itsme@devingaffney.com>
2022-04-29 14:57:04 -0700

    CHECK-1795 more fixes

--- app/main/lib/text_similarity.py
+++ app/main/lib/text_similarity.py
@@ -10,7 +10,7 @@ def add_text(body, doc_id):
   for model_key in body.pop("models", []):
     if model_key != 'elasticsearch':
       model = SharedModel.get_client(model_key)
-      vector = model.get_shared_model_response(request.json['text'])
+      vector = model.get_shared_model_response(body['content'])
       documents.append(store_document(dict(**body, **{'model': model_key, 'vector_'+str(len(vector)): vector}), doc_id))
     else:
       documents.append(store_document(dict(**body, **{'model': model_key}), doc_id))
