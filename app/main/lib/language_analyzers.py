8e1282d46f973a086962f7b9447eae8a8e116dd0
Devin Gaffney <itsme@devingaffney.com>
2023-01-17 16:54:35 -0800

    Meedan 2771 fail loudly (#275)
    * CHECK-2771 add new analyzers, add error logging when not stored
    
    * CHECK-2771 add new analyzers, add error logging when not stored
    
    * CHECK-2771 add new analyzers, add error logging when not stored
    
    * CHECK-2771 fix formatting
    
    * CHECK-2771 small tweak on analyzers...
    
    * add inside analysis clause
    
    * remove keywords clauses
    
    * allow for none language values
    
    * allow for none language values
    
    * drop whitespace

--- app/main/lib/language_analyzers.py
+++ app/main/lib/language_analyzers.py
@@ -1,7 +1,7 @@
 import json
 from elasticsearch import Elasticsearch
 from flask import request, current_app as app
-SUPPORTED_LANGUAGES = ["en", "pt", "es", "hi", "bn", "pt-br"]
+SUPPORTED_LANGUAGES = ["en", "pt", "es", "hi", "bn", "pt-br", "ar", "fr", "de", "cjk", "id"]
 #via https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis-lang-analyzer.html#bengali-analyzer
 SETTINGS_BY_LANGUAGE = {
   "en": {
@@ -160,6 +160,142 @@ SETTINGS_BY_LANGUAGE = {
             "bengali_stop",
             "bengali_stemmer",
             "asciifolding",
+          ]
+        }
+      }
+    }
+  },
+  "ar": {
+    "analysis": {
+      "filter": {
+        "arabic_stop": {
+          "type":       "stop",
+          "stopwords":  "_arabic_" 
+        },
+        "arabic_stemmer": {
+          "type":       "stemmer",
+          "language":   "arabic"
+        }
+      },
+      "analyzer": {
+        "rebuilt_ar": {
+          "tokenizer":  "standard",
+          "filter": [
+            "lowercase",
+            "decimal_digit",
+            "arabic_stop",
+            "arabic_normalization",
+            "arabic_stemmer"
+          ]
+        }
+      }
+    }
+  },
+  "fr": {
+    "analysis": {
+      "filter": {
+        "french_elision": {
+          "type":         "elision",
+          "articles_case": True,
+          "articles": [
+              "l", "m", "t", "qu", "n", "s",
+              "j", "d", "c", "jusqu", "quoiqu",
+              "lorsqu", "puisqu"
+            ]
+        },
+        "french_stop": {
+          "type":       "stop",
+          "stopwords":  "_french_" 
+        },
+        "french_stemmer": {
+          "type":       "stemmer",
+          "language":   "light_french"
+        }
+      },
+      "analyzer": {
+        "rebuilt_fr": {
+          "tokenizer":  "standard",
+          "filter": [
+            "french_elision",
+            "lowercase",
+            "french_stop",
+            "french_stemmer"
+          ]
+        }
+      }
+    }
+  },
+  "de": {
+    "analysis": {
+      "filter": {
+        "german_stop": {
+          "type":       "stop",
+          "stopwords":  "_german_" 
+        },
+        "german_stemmer": {
+          "type":       "stemmer",
+          "language":   "light_german"
+        }
+      },
+      "analyzer": {
+        "rebuilt_de": {
+          "tokenizer":  "standard",
+          "filter": [
+            "lowercase",
+            "german_stop",
+            "german_normalization",
+            "german_stemmer"
+          ]
+        }
+      }
+    }
+  },
+  "cjk": {
+    "analysis": {
+      "filter": {
+        "english_stop": {
+          "type":       "stop",
+          "stopwords":  [
+            "a", "and", "are", "as", "at", "be", "but", "by", "for",
+            "if", "in", "into", "is", "it", "no", "not", "of", "on",
+            "or", "s", "such", "t", "that", "the", "their", "then",
+            "there", "these", "they", "this", "to", "was", "will",
+            "with", "www"
+          ]
+        }
+      },
+      "analyzer": {
+        "rebuilt_cjk": {
+          "tokenizer":  "standard",
+          "filter": [
+            "cjk_width",
+            "lowercase",
+            "cjk_bigram",
+            "english_stop"
+          ]
+        }
+      }
+    }
+  },
+  "id": {
+    "analysis": {
+      "filter": {
+        "indonesian_stop": {
+          "type":       "stop",
+          "stopwords":  "_indonesian_" 
+        },
+        "indonesian_stemmer": {
+          "type":       "stemmer",
+          "language":   "indonesian"
+        }
+      },
+      "analyzer": {
+        "rebuilt_id": {
+          "tokenizer":  "standard",
+          "filter": [
+            "lowercase",
+            "indonesian_stop",
+            "indonesian_stemmer"
           ]
         }
       }
