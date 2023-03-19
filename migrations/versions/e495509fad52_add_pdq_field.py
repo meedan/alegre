a11697517931b3d6befbc163643b41abab45a703
computermacgyver <computermacgyver@gmail.com>
2023-03-19 21:49:19 +0000

    Minor clean up * Remove unused io import in image_hash.py * Remove duplicate alter_column command in migration

--- migrations/versions/e495509fad52_add_pdq_field.py
+++ migrations/versions/e495509fad52_add_pdq_field.py
@@ -23,7 +23,6 @@ def upgrade():
                existing_type=sa.BIGINT(),
                nullable=True)
     op.create_index(op.f('ix_images_pdq'), 'images', ['pdq'], unique=False)
-    op.alter_column('images', 'phash', nullable=True)
 
     # ### end Alembic commands ###
 
@@ -35,6 +34,5 @@ def downgrade():
                existing_type=sa.BIGINT(),
                nullable=False)
     op.drop_column('images', 'pdq')
-    op.alter_column('images', 'phash', nullable=False)
 
     # ### end Alembic commands ###
