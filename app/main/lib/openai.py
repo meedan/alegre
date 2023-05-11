446177b2fcaa1a26093b3bf57aefc3f7c460cb6b
ahmednasserswe <ahmed.nasser.swe@gmail.com>
2023-05-11 13:36:06 +0300

    code climate styling comments fixes

--- app/main/lib/openai.py
+++ app/main/lib/openai.py
@@ -8,20 +8,20 @@ PREFIX_OPENAI = "openai-"
 EMBEDDING_CACHE_DEFAULT = 60*60*24*7 #7 days (value is in seconds)
 def retrieve_openai_embeddings(text, model_key):
 
-    r = redis.Redis(host=app.config['REDIS_HOST'], port=app.config['REDIS_PORT'], db=app.config['REDIS_DATABASE'])
-    text=text.strip()
-    textmd5=hashlib.md5(text.encode('utf-8')).hexdigest()
+    r_cache = redis.Redis(host=app.config['REDIS_HOST'], port=app.config['REDIS_PORT'], db=app.config['REDIS_DATABASE'])
+    text = text.strip()
+    textmd5 = hashlib.md5(text.encode('utf-8')).hexdigest()
     key = f"cache_{model_key}_{textmd5}"
-    val_from_cache=r.get(key)
-    if val_from_cache!=None:
+    val_from_cache = r_cache.get(key)
+    if val_from_cache != None:
         return pickle.loads(val_from_cache)
     openai.api_key = app.config['OPENAI_API_KEY']
     app.logger.info(f"Calling OpenAI API")
     model_key_without_openai_prefix = model_key[len(PREFIX_OPENAI):]
     try:
         embeddings = openai.embeddings_utils.get_embedding(text, engine=model_key_without_openai_prefix)
-        r.set(key,pickle.dumps(embeddings))
-        r.expire(key,EMBEDDING_CACHE_DEFAULT)
+        r_cache.set(key,pickle.dumps(embeddings))
+        r_cache.expire(key,EMBEDDING_CACHE_DEFAULT)
     except Exception as e:
         app.logger.error(f"Unable to retreieve OpenAI embeddings. Error was {e}")
         embeddings=None    
