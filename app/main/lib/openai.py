241e2e46dd0f099826d87a70b09b4b54fa3b778c
ahmednasserswe <ahmed.nasser.swe@gmail.com>
2023-05-13 02:23:17 +0200

    code climate  styling fixes

--- app/main/lib/openai.py
+++ app/main/lib/openai.py
@@ -7,13 +7,13 @@ import pickle
 PREFIX_OPENAI = "openai-"
 EMBEDDING_CACHE_DEFAULT = 60*60*24*7 #7 days (value is in seconds)
 def retrieve_openai_embeddings(text, model_key):
-
+    """Return the vector embeddings of text using OpenAI API """
     r_cache = redis.Redis(host=app.config['REDIS_HOST'], port=app.config['REDIS_PORT'], db=app.config['REDIS_DATABASE'])
     text = text.strip()
     textmd5 = hashlib.md5(text.encode('utf-8')).hexdigest()
     key = f"cache_{model_key}_{textmd5}"
     val_from_cache = r_cache.get(key)
-    if val_from_cache != None:
+    if val_from_cache is not None:
         return pickle.loads(val_from_cache)
     openai.api_key = app.config['OPENAI_API_KEY']
     app.logger.info(f"Calling OpenAI API")
@@ -22,7 +22,7 @@ def retrieve_openai_embeddings(text, model_key):
         embeddings = openai.embeddings_utils.get_embedding(text, engine=model_key_without_openai_prefix)
         r_cache.set(key, pickle.dumps(embeddings))
         r_cache.expire(key, EMBEDDING_CACHE_DEFAULT)
-    except Exception as e:
-        app.logger.error(f"Unable to retreieve OpenAI embeddings. Error was {e}")
+    except Exception as caught_exception:
+        app.logger.error(f"Unable to retreieve OpenAI embeddings. Error was {caught_exception}")
         embeddings = None
     return embeddings
