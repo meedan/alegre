01de7c555afdd236a532022cc97f8cc5c3111d91
Devin Gaffney <itsme@devingaffney.com>
2023-07-07 09:45:28 -0700

    CV2-3374 update scoring mechanism

--- app/main/lib/shared_models/audio_model.py
+++ app/main/lib/shared_models/audio_model.py
@@ -147,7 +147,7 @@ class AudioModel(SharedModel):
                     SELECT id, doc_id, chromaprint_fingerprint, url, context, get_audio_chromaprint_score(chromaprint_fingerprint, :chromaprint_fingerprint)
                     AS score FROM audios
                   ) f
-                  WHERE score >= :threshold
+                  WHERE score <= :threshold
                   AND 
                   """+context_query+"""
                   ORDER BY score DESC
@@ -159,7 +159,7 @@ class AudioModel(SharedModel):
                     SELECT id, doc_id, chromaprint_fingerprint, url, context, get_audio_chromaprint_score(chromaprint_fingerprint, :chromaprint_fingerprint)
                     AS score FROM audios
                   ) f
-                  WHERE score >= :threshold
+                  WHERE score <= :threshold
                   ORDER BY score DESC
                 """
             matches = db.session.execute(text(cmd), dict(**{
@@ -172,6 +172,7 @@ class AudioModel(SharedModel):
                 row = dict(zip(keys, values))
                 # row["score"] = get_score(row["chromaprint_fingerprint"], chromaprint_fingerprint, threshold)
                 row["model"] = "audio"
+                row["score"] = 1-row["score"]
                 rows.append(row)
             return rows
         except Exception as e:
