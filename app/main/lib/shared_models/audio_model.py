cd0adc98358cd8cd9c062ab8024e51f937a56727
computermacgyver <computermacgyver>
2023-07-14 10:55:28 +0900

    Update tests - expect them to fail

--- app/main/lib/shared_models/audio_model.py
+++ app/main/lib/shared_models/audio_model.py
@@ -147,7 +147,7 @@ class AudioModel(SharedModel):
                     SELECT id, doc_id, chromaprint_fingerprint, url, context, get_audio_chromaprint_score(chromaprint_fingerprint, :chromaprint_fingerprint)
                     AS score FROM audios
                   ) f
-                  WHERE score <= :threshold
+                  WHERE score >= :threshold
                   AND 
                   """+context_query+"""
                   ORDER BY score DESC
@@ -159,7 +159,7 @@ class AudioModel(SharedModel):
                     SELECT id, doc_id, chromaprint_fingerprint, url, context, get_audio_chromaprint_score(chromaprint_fingerprint, :chromaprint_fingerprint)
                     AS score FROM audios
                   ) f
-                  WHERE score <= :threshold
+                  WHERE score >= :threshold
                   ORDER BY score DESC
                 """
             matches = db.session.execute(text(cmd), dict(**{
@@ -215,7 +215,7 @@ class AudioModel(SharedModel):
         audio, temporary = self.get_audio(task)
         context = self.get_context_for_search(task)
         if audio:
-            threshold = 1-task.get('threshold', 0.0)
+            threshold = task.get('threshold', 0.0)
             matches = self.search_by_hash_value(audio.chromaprint_fingerprint, threshold, context)
             limit = task.get("limit")
             if temporary:
