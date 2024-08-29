1752e0c15dec2920b41423cd59bc9e90fd5c003a
ahmednasserswe <ahmed.nasser.swe@gmail.com>
2024-08-29 20:34:39 +0200

    creating convert_text_annotation_to_json in image_ocr_controller.py to convert text_annotations to json

--- app/main/controller/image_ocr_controller.py
+++ app/main/controller/image_ocr_controller.py
@@ -4,8 +4,6 @@ from flask_restplus import Resource, Namespace, fields
 from google.cloud import vision
 import tenacity
 import json
-from google.protobuf.json_format import MessageToJson
-
 
 from app.main.lib.google_client import get_credentialed_google_client
 
@@ -20,6 +18,18 @@ def _after_log(retry_state):
 CLIENT = get_credentialed_google_client(vision.ImageAnnotatorClient)
 @api.route('/')
 class ImageOcrResource(Resource):
+    def convert_text_annotation_to_json(self, text_annotation):
+        text_json = {}
+        text_json['description'] = text_annotation.description
+        text_json['locale'] = text_annotation.locale
+        text_json['bounding_poly'] = []
+        for a_vertice in text_annotation.bounding_poly.vertices:
+            vertice_json = {}
+            vertice_json['x'] = a_vertice.x
+            vertice_json['y'] = a_vertice.y
+            text_json['bounding_poly'] += [vertice_json]
+        text_json = json.dumps(text_json)
+        return text_json
     @api.response(200, 'text successfully extracted.')
     @api.doc('Perform text extraction from an image')
     @api.doc(params={'url': 'url of image to extract text from'})
@@ -39,9 +49,8 @@ class ImageOcrResource(Resource):
         if not texts:
             return
 
-        res_dict = MessageToDict(response, preserving_proto_field_name=True)
         app.logger.info(
-            f"[Alegre OCR] [image_uri {image.source.image_uri}] Image OCR response package looks like {json.dumps(res_dict)}")
+            f"[Alegre OCR] [image_uri {image.source.image_uri}] Image OCR response package looks like {self.convert_text_annotation_to_json(texts[0])}")
 
         return {
             'text': texts[0].description
