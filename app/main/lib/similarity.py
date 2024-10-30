4a3aff4e1e82ca99b50796e9fd88260cd978db9b
Devin Gaffney <itsme@devingaffney.com>
2024-10-30 05:25:19 -0700

    CV2-5050 re-embed item on add event

--- app/main/lib/similarity.py
+++ app/main/lib/similarity.py
@@ -133,7 +133,7 @@ def callback_add_item(item, similarity_type):
     if function:
         response = function(item)
         app.logger.info(f"[Alegre Similarity] CallbackAddItem: [Item {item}, Similarity type: {similarity_type}] Response looks like {response}")
-        return response
+        return {"item": response}
     else:
         app.logger.warning(f"[Alegre Similarity] InvalidCallbackAddItem: [Item {item}, Similarity type: {similarity_type}] No response")
 
