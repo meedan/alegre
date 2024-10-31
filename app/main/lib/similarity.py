83e6360efb9170fe1e780091f63cb969a8e75388
Devin Gaffney <itsme@devingaffney.com>
2024-10-31 07:49:28 -0700

    CV2-5050 re-embed item on add event (#465)
    * CV2-5050 re-embed item on add event
    
    * because of course for some reason platform doesnt work all the sudden in travis
    
    * bump docker compose version maybe
    
    * revert hacks
    
    * minor tweak per pr comment

--- app/main/lib/similarity.py
+++ app/main/lib/similarity.py
@@ -131,7 +131,7 @@ def callback_add_item(item, similarity_type):
     elif similarity_type == "text":
         function = callback_add_text
     if function:
-        response = function(item)
+        response = {"item": function(item)}
         app.logger.info(f"[Alegre Similarity] CallbackAddItem: [Item {item}, Similarity type: {similarity_type}] Response looks like {response}")
         return response
     else:
