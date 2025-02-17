b677ccc69c97775474ef806eb3b8e7225fa24fa5
ahmednasserswe <ahmed.nasser.swe@gmail.com>
2025-02-17 17:20:24 +0100

    mocking cache in `test_openai_get_document_body` and `test_retrieve_openai_embeddings_calls_openai_api`

--- app/test/test_openai.py
+++ app/test/test_openai.py
@@ -49,7 +49,9 @@ class TestRetrieveOpenAIEmbeddings(unittest.TestCase):
             mock_retrieve_openai_embeddings.assert_called_once_with(self.test_content_sample['content'], "openai-text-embedding-ada-002")
             self.assertEqual({'text': 'this is a test', 'models': ['openai-text-embedding-ada-002'], 'content': 'let there be content', 'model': 'openai-text-embedding-ada-002', 'vector_openai-text-embedding-ada-002': self.test_content_embedding_true_value, 'model_openai-text-embedding-ada-002': 1}, result)
     def test_openai_get_document_body(self):
-        with patch('app.main.lib.text_similarity.retrieve_openai_embeddings') as mock_retrieve_openai_embeddings:
+        with patch('app.main.lib.openai.redis_client.get_client') as mock_redis_client:
+            mock_redis = mock_redis_client.return_value
+            mock_redis.get.return_value = None  # Ensure cache is empty
             result = get_document_body(self.test_content_sample)
             self.assertEqual({'text': 'this is a test', 'models': ['openai-text-embedding-ada-002'], 'content': 'let there be content', 'model': 'openai-text-embedding-ada-002', 'vector_openai-text-embedding-ada-002': self.test_content_embedding_true_value, 'model_openai-text-embedding-ada-002': 1}, result)
     def test_retrieve_openai_embeddings_calls_openai_api(self):
